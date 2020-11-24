dda55b3bc3df88f75ee6a34d108934cfde7ae38f
Gino Marceca <gino.marceca@epfl.ch>
2020-11-24 16:18:14 +0100

    update

--- setup.py
+++ setup.py
@@ -8,21 +8,17 @@ from utime import __version__
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read().replace('.. :changelog:', '')
-
 with open("requirements.txt") as req_file:
     requirements = list(filter(None, req_file.read().split("\n")))
 
 setup(
     name='utime',
     version=__version__,
-    description='A deep learning framework for automatic PSG sleep analysis.',
-    long_description=readme + "\n\n" + history,
-    author='Mathias Perslev',
-    author_email='map@di.ku.dk',
-    url='https://github.com/perslev/U-Time',
-    license="LICENSE.txt",
+    description='A deep learning framework for automatic plasma confinement states detection.',
+    long_description=readme,
+    author='Gino Marceca',
+    author_email='gino.marceca@epfl.ch',
+    url='https://github.com/gmarceca/UTime-Plasma-States',
     packages=["utime"],
     package_dir={'utime':
                  'utime'},
@@ -35,6 +31,5 @@ setup(
     classifiers=['Environment :: Console',
                  'Operating System :: POSIX',
                  'Programming Language :: Python :: 3.6',
-                 'Programming Language :: Python :: 3.7'
-                 'License :: OSI Approved :: MIT License']
+                 'Programming Language :: Python :: 3.7']
 )
